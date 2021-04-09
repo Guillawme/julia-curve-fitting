ccd8dd8a09766cc213414018a82f01526d3b4ec3
Guillaume Gaullier <guillaume@gaullier.org>
2021-04-09 13:10:44 +0200

    Added the capability to read remote data files by URL

--- curve-fitting.jl
+++ curve-fitting.jl
@@ -18,11 +18,13 @@ begin
 	using Chain
 	using CSV
 	using DataFrames
+	using HTTP
 	using LsqFit
 	using Measurements
 	using Plots
 	using PlutoUI
 	using Statistics
+	using URIs
 end
 
 # ╔═╡ 412eedac-658a-11eb-2326-93e5bf3d1a2c
@@ -40,13 +42,20 @@ The following reference explains very well the theory of equilibrium binding exp
 md"""
 ## Load data
 
-The data file must be in CSV format. The first row is assumed to contain column names. The first column is assumed to be the $X$ values, all other columns are assumed to be replicate $Y$ values that will be averaged (fitting will be done against the mean values, weighted by their standard deviations). In addition, there must not be any row with an $X = 0$ value (this would result in an error when attempting to plot with a logarithmic scale). I always perform two measurements at $X = 0$, and I always sort rows by descending $X$ values, so this notebook automatically skips the last two rows of the CSV file; adjust accordingly if you don't measure at $X = 0$ and want to keep all rows (see section [Data processing](#1884912a-6aeb-11eb-2b4a-d14d4a321dc5) below). The data does *not* need to be scaled such that $Y$ takes values between $0$ and $1$: the binding models can account for arbitrary minimum and maximum $Y$ values (see section [Model functions](#663a4cae-658a-11eb-382f-cf256c08c9d1) below). Scaling data will hide differences in signal change between datasets, so it should never be done "blindly"; always look at the raw data.
+The data file must be in CSV format. The first row is assumed to contain column names. The first column is assumed to be the $X$ values, all other columns are assumed to be replicate $Y$ values that will be averaged (fitting will be done against the mean values, weighted by their standard deviations). In addition, there must not be any row with an $X = 0$ value (this would result in an error when attempting to plot with a logarithmic scale). I always perform two measurements at $X = 0$, and I always sort rows by descending $X$ values, so this notebook automatically skips the last two rows of the CSV file; adjust accordingly if you don't measure at $X = 0$ and want to keep all rows (see section [Data processing](#1884912a-6aeb-11eb-2b4a-d14d4a321dc5) below). The data does *not* need to be scaled such that $Y$ takes values between $0$ and $1$: the binding models can account for arbitrary minimum and maximum $Y$ values (see section [Model functions](#663a4cae-658a-11eb-382f-cf256c08c9d1) below). Scaling data will hide differences in signal change between datasets, while these differences may tell you something about the system under study, so scaling should never be done "blindly"; always look at the raw data.
 
-Indicate below which data file to process:
+Indicate below which data files to process:
 
 - if the path given is not absolute, it is assumed to be relative to the notebook file (wherever the notebook is located)
 - a list of files can be provided with one file path per line and separated by commas
+- files can be located by local path or URL, but each type of location should be in the dedicated list
 """
+
+# ╔═╡ a2c02fcf-9402-4938-bc3d-819b45a66afa
+dataURLs = [
+	#"https://raw.githubusercontent.com/Guillawme/julia-curve-fitting/main/datasets/dataset_010.csv",
+	#"https://raw.githubusercontent.com/Guillawme/julia-curve-fitting/main/datasets/dataset_011.csv"
+]
 
 # ╔═╡ 14baf100-660f-11eb-1380-ebf4a860eed8
 dataFiles = [
@@ -54,10 +63,21 @@ dataFiles = [
 	"datasets/dataset_005.csv"
 ]
 
+# ╔═╡ 2ce72e97-0133-4f15-bf1d-7fd04ccf3102
+md"""
+**Number of rows to ignore at the end of files:** $(@bind footerRows PlutoUI.NumberField(0:30, default = 2))
+"""
+
 # ╔═╡ 214acce6-6ae8-11eb-3abf-492e50140317
 md"""
 Your data should appear below shortly, check that it looks normal. In addition to the columns present in your CSV file, you should see three columns named `mean`, `std` and `measurement` (these values will be used for fitting and plotting).
 """
+
+# ╔═╡ d5b0e2a1-865c-489c-9d0d-c4ae043828fb
+# By defaults, use file names and URLs to identify datasets.
+datasetNames = vcat(dataURLs, dataFiles)
+# But one can also use custom names.
+#datasetNames = ["Monday", "Tuesday", "Wednesday", "Thursday"]
 
 # ╔═╡ 5c5e0392-658a-11eb-35be-3d940d4504cb
 md"""
@@ -111,6 +131,16 @@ The fit residuals should follow a random normal distribution around $0$. If they
 In either case, your best option is to design a new experiment and collect new data.
 """
 
+# ╔═╡ 5a36fc3f-ce74-42c8-8284-19321e0d687f
+md"""
+#### Scatter plot
+"""
+
+# ╔═╡ 5392d99b-70f9-48cd-90b4-58cba5fc9681
+md"""
+#### Histogram
+"""
+
 # ╔═╡ be17b97e-663a-11eb-2158-a381c19ece3f
 md"""
 ## Numerical results
@@ -139,43 +169,13 @@ PlutoUI.TableOfContents()
 # ╔═╡ 1884912a-6aeb-11eb-2b4a-d14d4a321dc5
 md"### Data processing"
 
-# ╔═╡ 992c86a2-6b13-11eb-1e00-95bdff2736d0
+# ╔═╡ 4f4b580d-507c-4ad0-b1d5-5967c8ed829e
 md"""
-The `processData()` function loads one data file, computes the mean and standard deviation of replicates, defines measurements as mean ± std, and returns a DataFrame containing all the data.
+The `commonProcessing()` function computes the mean and standard deviation of replicates, defines measurements as mean ± std, and returns a DataFrame containing all the data. It is used by all methods of the following `processData()` function, which handle various inputs (path to a local file, URL to a remote file, loaded CSV file, loaded DataFrame).
 """
 
-# ╔═╡ 1fe4d112-6a11-11eb-37a6-bf95fbe032b1
-function processData(dataFile::String)
-	df = @chain dataFile begin
-		CSV.File(footerskip=2)
-		DataFrame()
-		# Rename column 1, so we can always call it by the
-		# same name regardless of its name in the input file.
-		rename(1 => :concentration)
-		@aside cols = ncol(_)
-		transform(
-			# Calculate mean and stddev of replicates
-			# (all columns in input except first one).
-			AsTable(2:cols) => ByRow(mean) => :mean,
-			AsTable(2:cols) => ByRow(std) => :std
-		)
-		transform(
-			# Mean and stddev together define a measurement
-			# (this is only for plotting; fitting uses the two
-			# original columns separately).
-			[:mean, :std] => ByRow(measurement) => :measurement
-		)
-	end
-	return df
-end
-
-# ╔═╡ 247c2416-6c67-11eb-01df-8dac01cdbf8f
-md"""
-This functions should also work if passed an already loaded data frame (for example, if the user wants to load data and pre-process it in a different way before averaging replicates):
-"""
-
-# ╔═╡ 36ebe112-6c66-11eb-11f0-7fdb946865e4
-function processData(data::DataFrame)
+# ╔═╡ 5eb607c7-172b-4a6c-a815-acbc195108f0
+function commonProcessing(data::DataFrame)
 	df = @chain data begin
 		# Rename column 1, so we can always call it by the
 		# same name regardless of its name in the input file.
@@ -197,8 +197,71 @@ function processData(data::DataFrame)
 	return df
 end
 
+# ╔═╡ 992c86a2-6b13-11eb-1e00-95bdff2736d0
+md"""
+The `processData()` function loads one data file, computes the mean and standard deviation of replicates, defines measurements as mean ± std, and returns a DataFrame containing all the data.
+"""
+
+# ╔═╡ 1fe4d112-6a11-11eb-37a6-bf95fbe032b1
+function processData(dataFile::String)
+	df = @chain dataFile begin
+		CSV.File(footerskip=footerRows)
+		DataFrame()
+		commonProcessing()
+	end
+	return df
+end
+
+# ╔═╡ 5cc812af-4f8e-444b-9652-fb063cc6be06
+md"""
+This functions should also work if passed an URL to a remote file:
+"""
+
+# ╔═╡ 7c2af794-a0ed-4827-bc8e-1f15ad205eca
+function processData(dataFile::URI)
+	df = @chain dataFile begin
+		HTTP.get(_).body
+		CSV.File(footerskip=footerRows)
+		DataFrame()
+		commonProcessing()
+	end
+	return df
+end
+
+# ╔═╡ a08d1b29-fa5e-4312-977c-39b050de4516
+md"""
+This functions should also work if passed an already loaded CSV file:
+"""
+
+# ╔═╡ 0181a0b1-ec6c-4325-8b3a-851a3fe33846
+function processData(dataFile::CSV.File)
+	df = @chain dataFile begin
+		DataFrame()
+		commonProcessing()
+	end
+	return df
+end
+
+# ╔═╡ 247c2416-6c67-11eb-01df-8dac01cdbf8f
+md"""
+This functions should also work if passed an already loaded data frame (for example, if the user wants to load data and pre-process it in a different way before averaging replicates):
+"""
+
+# ╔═╡ 36ebe112-6c66-11eb-11f0-7fdb946865e4
+function processData(data::DataFrame)
+	return commonProcessing(data)
+end
+
 # ╔═╡ d904fd76-6af1-11eb-2352-837e03072137
-dfs = [ processData(df) for df in dataFiles ]
+begin
+	allData = vcat(URI.(dataURLs), dataFiles)
+	dfs = [ processData(df) for df in allData ]
+end
+
+# ╔═╡ 0e8af3be-7ae7-4ec2-8d7a-670878cd52ee
+md"""
+We will need to keep track of dataset names. You can edit them here if you want to use something more meaningful than the file name or URL; changes will propagate to plot legends. This list **must** contain the same number of elements as you have datasets: **$(length(allData))** in the present case.
+"""
 
 # ╔═╡ 8e105fae-6aec-11eb-1471-83aebb776241
 md"### Plotting"
@@ -248,7 +311,7 @@ md"The `initResidualHistogram()` function initializes a histogram, the `plotOneR
 function initResidualHistogram()
 	histogram(
 		xlabel = "Fit residual",
-		ylabel = "Frequency",
+		ylabel = "Count",
 		legend = :topleft
 	)
 	vline!([0], label = nothing, color = :red)
@@ -416,8 +479,8 @@ md"Perform fit of the selected model to the measurements' mean values using init
 
 # ╔═╡ 47426056-6af2-11eb-17f8-6d27d35003ca
 begin
-	fits = Vector{LsqFit.LsqFitResult}(undef, length(dataFiles))
-	for (df, initialValues, i) in zip(dfs, initialParams, 1:length(dataFiles))
+	fits = Vector{LsqFit.LsqFitResult}(undef, length(allData))
+	for (df, initialValues, i) in zip(dfs, initialParams, 1:length(allData))
 		if ncol(df) > 5
 			# If the dataset has more than 5 columns, it means it has
 			# replicate Y values, so we weight the fit by their stddev.
@@ -441,7 +504,7 @@ end
 # ╔═╡ 264bf9ec-6af5-11eb-1ffd-79fb3466f596
 begin
 	dataPlot = initMainPlot()
-	for (df, fit, title, initialVals) in zip(dfs, fits, dataFiles, initialParams)
+	for (df, fit, title, initialVals) in zip(dfs, fits, datasetNames, initialParams)
 		plotOneDataset!(dataPlot, df, fit, title, showInitialFit, initialVals)
 	end
 	dataPlot
@@ -450,7 +513,7 @@ end
 # ╔═╡ a951b5dc-6af7-11eb-2401-5d11a14e3067
 begin
 	residualPlot = initResidualPlot()
-	for (df, fit, title) in zip(dfs, fits, dataFiles)
+	for (df, fit, title) in zip(dfs, fits, datasetNames)
 		plotOneResiduals!(residualPlot, df, fit, title)
 	end
 	residualPlot
@@ -459,7 +522,7 @@ end
 # ╔═╡ 7625d41a-dab1-4b10-947c-3667c03f85aa
 begin
 	residualHistogram = initResidualHistogram()
-	for (df, fit, title) in zip(dfs, fits, dataFiles)
+	for (df, fit, title) in zip(dfs, fits, datasetNames)
 		plotOneResidualsHistogram!(residualHistogram, df, fit, title)
 	end
 	residualHistogram
@@ -468,7 +531,7 @@ end
 # ╔═╡ 2109f516-6b99-11eb-05a0-99b9ecfd0f9d
 PlutoUI.with_terminal() do
 	println("Dataset\t\t\t\tSum of squared residuals")
-	for (dataset, fit) in zip(dataFiles, fits)
+	for (dataset, fit) in zip(datasetNames, fits)
 		println(
 			split(dataset, "/")[end],
 			"\t\t",
@@ -500,7 +563,7 @@ PlutoUI.with_terminal() do
 	if length(initialParams[1]) == 3
 		# No Hill coefficient to report.
 		println("Dataset\t\t\t\tKd\t\t\t\tSmin\t\t\tSmax")
-		for (dataset, fit, stderr) in zip(dataFiles, fits, paramsStdErrors)
+		for (dataset, fit, stderr) in zip(datasetNames, fits, paramsStdErrors)
 			println(
 				split(dataset, "/")[end],
 				"\t\t",
@@ -520,7 +583,7 @@ PlutoUI.with_terminal() do
 	elseif length(initialParams[1]) == 4
 		# There is a Hill coefficient to report.
 		println("Dataset\t\t\t\tKd\t\t\t\tSmin\t\t\tSmax\t\t\th")
-		for (dataset, fit, stderr) in zip(dataFiles, fits, paramsStdErrors)
+		for (dataset, fit, stderr) in zip(datasetNames, fits, paramsStdErrors)
 			println(
 				split(dataset, "/")[end],
 				"\t\t",
@@ -550,9 +613,13 @@ end
 # ╔═╡ Cell order:
 # ╟─412eedac-658a-11eb-2326-93e5bf3d1a2c
 # ╟─270cc0cc-660f-11eb-241e-b75746a39cc7
+# ╠═a2c02fcf-9402-4938-bc3d-819b45a66afa
 # ╠═14baf100-660f-11eb-1380-ebf4a860eed8
+# ╟─2ce72e97-0133-4f15-bf1d-7fd04ccf3102
 # ╟─214acce6-6ae8-11eb-3abf-492e50140317
 # ╟─d904fd76-6af1-11eb-2352-837e03072137
+# ╟─0e8af3be-7ae7-4ec2-8d7a-670878cd52ee
+# ╠═d5b0e2a1-865c-489c-9d0d-c4ae043828fb
 # ╟─5c5e0392-658a-11eb-35be-3d940d4504cb
 # ╟─3dd72c58-6b2c-11eb-210f-0b547bf38ebe
 # ╟─3da83f72-6a11-11eb-1a74-49b66eb39c96
@@ -561,7 +628,9 @@ end
 # ╟─264bf9ec-6af5-11eb-1ffd-79fb3466f596
 # ╟─4f4000b4-6b2c-11eb-015f-d76a0adda0a0
 # ╟─c50cf18c-6b11-11eb-07d3-0b8e332ec5bc
+# ╟─5a36fc3f-ce74-42c8-8284-19321e0d687f
 # ╟─a951b5dc-6af7-11eb-2401-5d11a14e3067
+# ╟─5392d99b-70f9-48cd-90b4-58cba5fc9681
 # ╟─7625d41a-dab1-4b10-947c-3667c03f85aa
 # ╟─be17b97e-663a-11eb-2158-a381c19ece3f
 # ╟─090347fc-6b8e-11eb-0e17-9d9d45749c0b
@@ -572,8 +641,14 @@ end
 # ╠═393b2f5e-6556-11eb-2119-cf7309ee7392
 # ╠═abc03f64-6a11-11eb-0319-ed7cea455cb5
 # ╟─1884912a-6aeb-11eb-2b4a-d14d4a321dc5
+# ╟─4f4b580d-507c-4ad0-b1d5-5967c8ed829e
+# ╠═5eb607c7-172b-4a6c-a815-acbc195108f0
 # ╟─992c86a2-6b13-11eb-1e00-95bdff2736d0
 # ╠═1fe4d112-6a11-11eb-37a6-bf95fbe032b1
+# ╟─5cc812af-4f8e-444b-9652-fb063cc6be06
+# ╠═7c2af794-a0ed-4827-bc8e-1f15ad205eca
+# ╟─a08d1b29-fa5e-4312-977c-39b050de4516
+# ╠═0181a0b1-ec6c-4325-8b3a-851a3fe33846
 # ╟─247c2416-6c67-11eb-01df-8dac01cdbf8f
 # ╠═36ebe112-6c66-11eb-11f0-7fdb946865e4
 # ╟─8e105fae-6aec-11eb-1471-83aebb776241
